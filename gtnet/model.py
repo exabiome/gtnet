49f054b6ea682fc182c190114e39ba9f05e2bdd2
Ali Zaidi <41302741+alibabaz@users.noreply.github.com>
2021-08-25 16:54:35 -0400

    Onnx fwd (#4)
    * rename to reflect real name of model
    
    * add dynamic batch size
    
    * commit to get updated model
    
    * correct model check
    
    * add softmax to model
    
    * commit to pull down softmax model
    
    * fwd pass with onnx model works - need to add command line arguments + return results in better fashion
    
    * command line arguments added - need to add output file or table
    
    * all changes implemented from PR review aside from modified rev-comp funciton
    
    * corrected with flake8
    
    * corrected reverse chunk/seq.py + removed extra items for testing, adding error logging
    
    * manually removed .ipynb + misplaced .gitignore
    
    * remove probability calc, move rcmap to be called only once, corrected chunking function to pad and reshape, remove extraneous notebook checkpoint folder
    
    * added items to gitignore, removed hardcoding in chunking fxn, modify get_dna_map to permit use of a different vocabulary string from user + create appropriate rcmap
    
    * added cli argument to receive a different vocab
    
    * removed extraneous files
    
    * removed comments + added docstrings
    
    * Update gtnet/sequence.py
    
    Co-authored-by: Andrew Tritt <ajtritt@lbl.gov>
    
    Co-authored-by: Andrew Tritt <ajtritt@lbl.gov>

--- gtnet/model.py
+++ gtnet/model.py
@@ -1,10 +1,14 @@
-import onnxruntime
+import onnxruntime as rt
 
 # assume this is filled in and is a path to an ONNX file
-PRETRAINED_MODEL_PATH = ...
+PRETRAINED_MODEL_PATH = 'data/bac120_r202.resnet50.genus.onnx'
 
+
+# read model_path and return model
 def load_model(model_path=None):
     if model_path is None:
+        print(f'a model path was not provided, \
+                using {PRETRAINED_MODEL_PATH} to proceed')
         model_path = PRETRAINED_MODEL_PATH
-    # read model_path and return model
-    pass
+    model = rt.InferenceSession(model_path)
+    return model
