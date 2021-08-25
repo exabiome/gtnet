18721c1e09ada1444a7612b62f056d245be2b3b2
Andrew Tritt <ajtritt@lbl.gov>
2021-08-25 14:03:17 -0700

    Add a function to get model path (#5)
    * rename directory
    
    * add a function for getting internal model path
    
    Co-authored-by: Ali Zaidi <41302741+alibabaz@users.noreply.github.com>

--- gtnet/model.py
+++ gtnet/model.py
@@ -1,10 +1,35 @@
+import os
+from pkg_resources import resource_filename
 import onnxruntime as rt
+
 
 # assume this is filled in and is a path to an ONNX file
 PRETRAINED_MODEL_PATH = 'data/bac120_r202.resnet50.genus.onnx'
 
 
-# read model_path and return model
+def _get_model_path(domain):
+    """
+    Get the path to the ONNX model file for a given domain
+
+    Parameters
+    -----------
+    domain: {'bacteria', 'archaea'}
+        The string indication which model to get the path for
+
+    Returns
+    -------
+    path: str
+        The absolute path to the ONNX model file
+    """
+    if domain == 'bacteria':
+        path = "bac120_r202.resnet50.genus.onnx"
+    elif domain == 'archaea':
+        raise ValueError("Model not available for archaea yet")
+    else:
+        raise ValueError("Unrecognized domain: '%s'" % domain)
+    return os.path.join(resource_filename(__name__, 'models'), path)
+
+
 def load_model(model_path=None):
     if model_path is None:
         print(f'a model path was not provided, \
