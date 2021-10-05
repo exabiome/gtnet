1f1bcf798363d3f30766469aad2c5a7f9d432600
Ali Zaidi <41302741+alibabaz@users.noreply.github.com>
2021-10-05 15:24:25 -0400

    Clean cli (#7)
    * init commit + moving cli args to gtnet-predict.py -- need to resolve issue with relative imports
    
    * added comments for sequence functions, modified cli + fxn parameter to handle domain

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ reqs = [
     'numpy',
     'scipy',
     'scikit-learn',
-    #'scikit-bio',
+    'scikit-bio',
     'hdmf',
 ]
 
@@ -57,7 +57,7 @@ setup_args = {
     'install_requires': reqs,
     'packages': pkgs,
     # 'package_dir': {'': 'src'},
-    'package_data': {'gtnet': ["data/*.onnx"]},
+    'package_data': {'gtnet': ["models/*.onnx"]},
     'classifiers': [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.5",
