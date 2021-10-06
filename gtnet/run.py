7adcb318a939bc25b125ce361a8390d04c7762e7
aaz <41302741+alibabaz@users.noreply.github.com>
2021-10-05 20:39:46 -0400

    cleaned up run.py & utils.py

--- gtnet/run.py
+++ gtnet/run.py
@@ -14,9 +14,9 @@ def predict(fasta_path, domain, vocab, **kwargs):
     model = load_model(domain)
     input_name = model.get_inputs()[0].name
     chars, basemap, rcmap = _get_DNA_map()
-    
+
     species_names = get_label_file()
-    
+
     preds = []
     for seq in get_sequences(fasta_path, basemap):
         # 1. fwd sequence is turned into bidirectional seq (chunked)
@@ -25,9 +25,9 @@ def predict(fasta_path, domain, vocab, **kwargs):
         # 2. pass chunks into model
         output = model.run(None, {input_name: bidir_seq.astype(np.int64)})[0]
         pred = get_species_pred(output)
-        
+
         # 3. extract species name based off model prediction
         species = species_names['species'][pred]
         preds.append(species)
-        
+
     pd.DataFrame(preds, columns=['species']).to_csv('predictions.csv', index=False)
--- gtnet/utils.py
+++ gtnet/utils.py
@@ -12,7 +12,7 @@ def get_label_file(domain='archaea'):
         path = 'arc_names.json'
     else:
         path = 'bac_names.json'
-    
+
     name_path = os.path.join(resource_filename(__name__, 'labels'), path)
     with open(name_path) as species_file:
         species_names = json.load(species_file)
