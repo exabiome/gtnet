9b83f0bad25321787f32436ce46391744d19b94b
aaz <41302741+alibabaz@users.noreply.github.com>
2021-10-11 19:27:40 -0400

    create gtnet.py, symlink gtnet, add command functionality in gtnet.py, move download_model to model.py, move prediction command to run.py

---
+++ bin/gtnet.py
@@ -0,0 +1,39 @@
+import sys
+from importlib import import_module
+
+
+class Command:
+    def __init__(self, module, doc):
+        ar = f'gtnet.{module}'.split('.')
+        self.pkg = '.'.join(ar[:-1])
+        self.func = ar[-1]
+        self.doc = doc
+
+    def get_func(self):
+        return getattr(import_module(self.pkg), self.func)
+
+
+command_dict = {
+    'download-models': Command('model.download_models',
+                               'Download models if not already available'),
+    'predict': Command('run.predict',
+                       'Predict taxonomy of provided sequence(s)'),
+}
+
+
+def print_help():
+    print('Usage: gtnet <command> [options]')
+    print('Available commands are:\n')
+    for c, f in command_dict.items():
+        nspaces = 16 - len(c)
+        print(f'    {c}' + ' '*nspaces + f.doc)
+    print()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print_help()
+    else:
+        cmd = sys.argv[1]
+        func = command_dict[cmd].get_func()
+        func(sys.argv[2:])
