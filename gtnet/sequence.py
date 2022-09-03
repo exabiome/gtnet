7569479d0371c43bb231c57ced79c89f6c1de1cc
Andrew Tritt <ajtritt@lbl.gov>
2022-09-03 00:09:18 -0700

    Add parallel data loading

--- gtnet/sequence.py
+++ gtnet/sequence.py
@@ -1,5 +1,9 @@
+import logging
+import multiprocessing as mp
+
 import numpy as np
 import skbio
+from skbio.sequence import DNA
 
 __all__ = ['_get_DNA_map',
            'get_sequences',
@@ -39,25 +43,99 @@ def get_revcomp_map(vocab):
     return rcmap
 
 
-def _get_DNA_map(vocab=None):
-    '''
-    create a DNA map with some redundancy so that we can
-    do base-complements with +/% operations.
-    Using this scheme, the complement of a base should be:
-    (base_integer + 9) % 18
-    chars[(basemap[ord('N')]+9)%18]
-    For this to work, bases need to be ordered as they are below
-    '''
-    if vocab is None:
-        vocab = ('ACYWSKDVNTGRMHB')
-    rcmap = get_revcomp_map(vocab)
-    basemap = np.zeros(128, dtype=np.uint8)
-    # reverse so we store the lowest for self-complementary codes
-    for i, c in reversed(list(enumerate(vocab))):
-        basemap[ord(c)] = i
-        basemap[ord(c.lower())] = i
-    basemap[ord('x')] = basemap[ord('X')] = basemap[ord('n')]
-    return vocab, basemap, rcmap
+class FastaSequenceEncoder:
+
+    def __init__(self, window, step, vocab=None, padval=None):
+        self.window = window
+        self.step = step
+        self.vocab, self.basemap, self.rcmap = self.get_dna_map(vocab=vocab)
+        if padval is None:
+            self.padval = next(i for i in range(len(self.vocab)) if self.vocab[i].upper() == 'N')
+        else:
+            self.padval = padval
+
+    def encode(self, seq):
+        if seq.dtype == np.dtype('S1'):
+            seq = seq.view(np.uint8)
+        elif seq.dtype == np.dtype('U1'):
+            seq = seq.astype('S').view(np.uint8)
+        elif seq.dtype != np.uint8:
+            raise ValueError('seq must be bytes or uint8')
+        fwd = self.basemap[seq]
+        rev = self.rcmap[fwd[::-1]]
+        starts = np.arange(0, fwd.shape[0], self.step)
+        ends = np.minimum(starts + self.window, fwd.shape[0])
+        batches = np.zeros((2 * len(starts), self.window), dtype=int) + self.padval
+        for i, (s, e) in enumerate(zip(starts, ends)):
+            l = e - s
+            batches[2 * i][:l] = fwd[s:e]
+            batches[2 * i + 1][:l] = rev[s:e]
+        return batches
+
+    @staticmethod
+    def get_dna_map(vocab=None):
+        '''
+        create a DNA map with some redundancy so that we can
+        do base-complements with +/% operations.
+        Using this scheme, the complement of a base should be:
+        (base_integer + 9) % 18
+        chars[(basemap[ord('N')]+9)%18]
+        For this to work, bases need to be ordered as they are below
+        '''
+        if vocab is None:
+            vocab = ('ACYWSKDVNTGRMHB')
+        rcmap = get_revcomp_map(vocab)
+        basemap = np.zeros(128, dtype=np.uint8)
+        # reverse so we store the lowest for self-complementary codes
+        for i, c in reversed(list(enumerate(vocab))):
+            basemap[ord(c)] = i
+            basemap[ord(c.lower())] = i
+        basemap[ord('x')] = basemap[ord('X')] = basemap[ord('n')]
+        return vocab, basemap, rcmap
+
+
+
+class FastaReader(mp.Process):
+
+    _done = 0xDEADBEEF
+
+    def __init__(self, encoder, *fastas):
+        self.encoder = encoder
+        self.fastas = fastas
+
+    def _target(self, q, fastas):
+        i = 0
+        for fa in fastas:
+            print("Reading sequence", i)
+            logging.info(f'loading {fa}')
+            for seq in skbio.read(fa, format='fasta', constructor=DNA, validate=False):
+                batches = self.encoder.encode(seq.values)
+                val = (fa, seq.metadata['id'], len(seq), batches)
+                q.put(val)
+            i += 1
+        q.put(self._done)
+
+    def read(self, fastas):
+        print("creating Queue")
+        self._q = mp.Queue()
+        self._proc = mp.Process(target=self._target, args=(self._q, fastas))
+        print("starting Process")
+        self._proc.start()
+        return self
+
+    def __iter__(self):
+        print("returning self")
+        return self
+
+    def __next__(self):
+        seq = None
+        while True:
+            seq = self._q.get()
+            if seq == self._done:
+                self._proc.join()
+                raise StopIteration
+            break
+        return seq
 
 
 def get_sequences(path, basemap):
---
+++ tests/test_sequence.py
@@ -0,0 +1,59 @@
+import multiprocessing as mp
+import tempfile
+
+import numpy as np
+
+from gtnet.sequence import FastaSequenceEncoder, FastaReader
+
+VOCAB = 'ACYWSKDVNTGRMHB'
+
+
+def get_fasta():
+    """TODO - modify this to write to a temp file and return the tempfile path"""
+    fl = [">seq1 Sulfolobus",
+          "AAGCTNGGGCATTTCAGGGTGAGCCCGGGCAATACAGGG",
+          ">seq2 Halobacteria",
+          "AAGCCTTGGCAGTGCAGGGTGAGCCGTGGCCGGGCACGGTA",
+          ">seq3 Salmonella",
+          "ACCGGTTGGCCGTTCAGGGTACAGGTTGGCCGTTCAGGGTAA",
+          ">seq4 E.coli",
+          "AAACCCTTGCCGTTACGCTTAAACCGAGGCCGGGACAC",
+          ">seq5 Y.pestis",
+          "AAACCCTTGCCGGTACGCTTAAACCATTGCCGGTACGCTT"]
+    _, path = tempfile.mkstemp()
+    with open(path, 'w') as f:
+        for l in fl:
+            print(l, file=f)
+    return path
+
+
+def test_encoder():
+    encoder = FastaSequenceEncoder(3, 2, vocab=VOCAB)
+    seq = np.array(list("ACTGNGTCNA"))
+    batch = encoder.encode(seq)
+    print(len(seq), batch.shape)
+    expected = np.array([
+        [  0,  1,  9],  # ACT
+        [  9,  8, 10],  # TNG
+        [  9, 10,  8],  # TGN
+        [ 10,  0,  1],  # GAC
+        [  8, 10,  9],  # NGT
+        [  1,  8,  1],  # CNC
+        [  9,  1,  8],  # TCN
+        [  1,  0, 10],  # CAG
+        [  8,  0,  8],  # NA
+        [ 10,  9,  8],  # GT
+    ])
+    np.testing.assert_array_equal(expected, batch)
+
+
+def test_reader():
+    encoder = FastaSequenceEncoder(8, 2, vocab=VOCAB)
+    reader = FastaReader(encoder)
+    fa = get_fasta()
+    i = 0
+    for seq in reader.read([fa]):
+        assert len(seq) == 4
+        i += 1
+    assert i == 5
+
