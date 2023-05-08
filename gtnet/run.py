0fb2399220e7eb9b8cf376e06586dd8ec86f6ca0
ajtritt <ajtritt@lbl.gov>
2023-05-08 16:43:53 -0700

    Remove old code

--- gtnet/run.py
+++ gtnet/run.py
@@ -1,103 +1,39 @@
-from .sequence import _get_DNA_map, batch_sequence, DNAEncoder
-from .utils import get_taxon_pred, get_config
-from .utils import get_logger, get_data_path
-import onnxruntime as rt
-import ruamel.yaml as yaml
+import argparse
+import json
+import logging
+import os
+import pickle
+import sys
+from time import time
+
 import numpy as np
 import pandas as pd
-import argparse
-import skbio
-from skbio.sequence import DNA
-import json
-import sys
-import os
 from pkg_resources import resource_filename
-
 import torch
+import torch.nn as nn
 
-
-def get_predictions(fasta_path, output_dest=None, **kwargs):
-    '''
-    Extracts associated configuration file for model to
-    instantiate model, label file and encoder
-
-    Takes a Fasta file and with each sequence in the file it
-    will break them into batches, run inference on that sequence
-    and determine the best prediction.
-
-    Records all predictions into a single DataFrame that is
-    written to `output_dest` or standard output if `output_dest` is not provided
-
-    Parameters
-    ----------
-    fasta_path : str
-        Path of single fasta file
-
-    output_dest : str, default=None
-        Path where final predictions will be deposited
-    '''
-    if fasta_path is None:
-        logging.error('The path provided is not a fasta file')
-        exit()
-
-    config = get_config()
-    taxon_table = pd.read_csv(config.taxa_df_path)
-    preds = []
-
-    model =  rt.InferenceSession(config.inf_model_path)
-    input_name = model.get_inputs()[0].name
-    encoder = DNAEncoder(config.chars)
-
-    for sequence in skbio.read(fasta_path, format='fasta',
-                               constructor=DNA, validate=False):
-        # 1. Turn full sequence into windowed batches
-        batches = batch_sequence(sequence=sequence,
-                                window=config.window,
-                                padval=config.pad_value,
-                                step=config.step,
-                                encoder=encoder)
-        batches = batches[:10]
-        # 2. pass chunks into model
-        output = model.run(None, {input_name: batches.astype(np.int64)})[0]
-        pred_idx = get_taxon_pred(output)
-
-        # 3. extract predicted row from taxon_table
-        taxon_pred = taxon_table.iloc[pred_idx]
-        preds.append(taxon_pred)
-
+from .sequence import FastaReader, FastaSequenceEncoder
+from .utils import get_logger, DeployPkg
 
 
 def _load_deploy_pkg():
-    deploy_dir = resource_filename(__name__, 'deploy_pkg')
-    path = lambda x: return os.path.join(deploy_dir, x)
-
-    files = ('taxa_table', 'nn_model', 'training_config')
-    # read manifest
-    # remap files in deploy_dir to be relative to where we are running
-
-    with open(path('manifest.json'), 'r') as f:
-        manifest = json.load(f)
-    for key in files:
-        manifest[key] = path(manifest[key])
+    pkg = DeployPkg()
 
     tmp_conf_model = dict()
-    for lvl_dat in manifest['conf_model']:
-        for k, v in lvl_dat:
-            if isinstance(v, list):
-                lvl_dat[k] = np.array(v)
+    for lvl_dat in pkg['conf_model']:
+        lvl_dat['taxa'] = np.array(lvl_dat['taxa'])
 
-        with open(lvl_dat.pop('model_path'), 'rb') as f:
-            lvl_dat['model'] = pickle.load(f)
+        lvl_dat['model'] = torch.jit.load(pkg.path(lvl_dat.pop('model')))
 
         tmp_conf_model[lvl_dat['level']] = lvl_dat
 
-    manifest['conf_model'] = tmp_conf_model
+    pkg['conf_model'] = tmp_conf_model
 
-    manifest['vocabulary'] = "".join(manifest['vocabulary'])
+    pkg['vocabulary'] = "".join(pkg['vocabulary'])
 
-    manifest['nn_model'] = torch.jit.load(manifest['nn_model'])
+    pkg['nn_model'] = torch.jit.load(pkg.path(pkg['nn_model']))
 
-    return manifest['nn_model'], manifest['conf_models'], manifest['training_config'], manifest['vocabulary']
+    return pkg['nn_model'], pkg['conf_model'], pkg['training_config'], pkg['vocabulary']
 
 
 class GPUModel(nn.Module):
@@ -116,37 +52,24 @@ def run_torchscript_inference(argv=None):
     """
     Convert a Torch model checkpoint to ONNX format
     """
-
-    import argparse
-    import logging
-    from time import time
-
-    import numpy as np
-    from numpy.lib.stride_tricks import sliding_window_view as swv
-    import pandas as pd
-    import ruamel.yaml as yaml
-    import skbio
-    from skbio.sequence import DNA
-
-    from .sequence import FastaReader, FastaSequenceEncoder
-
     desc = "Run predictions using ONNX"
     epi = ("")
 
     parser = argparse.ArgumentParser(description=desc, epilog=epi)
-    parser.add_argument('fastas', nargs='+', type=str, help='the Fasta files to do taxonomic classification on')
+    parser.add_argument('fasta', nargs='+', type=str, help='the Fasta files to do taxonomic classification on')
     parser.add_argument('-c', '--n_chunks', type=int, default=10000, help='the number of sequence chunks to process at a time')
     parser.add_argument('-F', '--fof', action='store_true', default=False, help='a file-of-files was passed in')
     parser.add_argument('-o', '--output', type=str, default=None, help='the output file to save classifications to')
-    if torch.cuda.is_available()
+    if torch.cuda.is_available():
         parser.add_argument('-g', '--gpu', action='store_true', default=False, help='Use GPU')
     parser.add_argument('-d', '--debug', action='store_true', default=False, help='print specific information about sequences')
 
     args = parser.parse_args(argv)
 
+    before = time()
     if args.fof:
-        with open(args.fastas[0], 'r') as f:
-            args.fastas = [s.strip() for s in f.readlines()]
+        with open(args.fasta[0], 'r') as f:
+            args.fasta = [s.strip() for s in f.readlines()]
 
     logger = get_logger()
     if args.debug:
@@ -156,15 +79,25 @@ def run_torchscript_inference(argv=None):
 
     model, conf_models, train_conf, vocab = _load_deploy_pkg()
 
+    window = train_conf['window']
+    step = train_conf['step']
+
+    encoder = FastaSequenceEncoder(window, step, vocab=vocab)
+    reader = FastaReader(encoder)
+
     if use_gpu:
         model = GPUModel(model, torch.device(0))
 
-    output_size = sum(len(lvl['taxa']) for lvl in conf_models)
+    output_size = sum(len(lvl['taxa']) for lvl in conf_models.values())
 
     seqnames = list()
     lengths = list()
     filepaths = list()
-    for file_path, seq_name, seq_len, seq_chunks in reader.read(args.fastas):
+    aggregated = list()
+
+    torch.set_grad_enabled(False)
+
+    for file_path, seq_name, seq_len, seq_chunks in reader.read(args.fasta):
         seqnames.append(seq_name)
         lengths.append(seq_len)
         filepaths.append(file_path)
@@ -174,20 +107,17 @@ def run_torchscript_inference(argv=None):
         # sum network outputs from all chunks
         for s in range(0, len(seq_chunks), args.n_chunks):
             e = s + args.n_chunks
-            #outputs += nn_sess.run(None, {'input': seq_chunks[s:e]})[0].sum(axis=0)
             outputs += model(torch.from_numpy(seq_chunks[s:e])).sum(dim=0)
         # divide by the number of seq_chunks we processed to get a mean output
         outputs /= len(seq_chunks)
 
         aggregated.append(outputs)
 
-    lengths = np.array(lengths)
+    lengths = torch.Tensor(lengths)
 
 
     # aggregate everything we just pulled from the fasta file
-    breakpoint()
-
-    all_levels_aggregated = np.array(aggregated)
+    all_levels_aggregated = torch.row_stack(aggregated)
 
     output_data = {'ID': seqnames}
     if len(args.fasta) > 1:
@@ -196,6 +126,7 @@ def run_torchscript_inference(argv=None):
     s = 0
     for lvl, e in zip(model.levels, model.parse):
         conf_model_info = conf_models[lvl]
+        taxa = conf_model_info['taxa']
         conf_model = conf_model_info['model']
 
         # determine the number of top k probabilities
@@ -204,18 +135,17 @@ def run_torchscript_inference(argv=None):
 
         logger.info(f'Getting {lvl} predictions')
         aggregated = all_levels_aggregated[:, s:e]
-        output_data[lvl] = conf_model['taxa'][np.argmax(aggregated, axis=1)]
+        output_data[lvl] = taxa[torch.argmax(aggregated, dim=1)]
 
         # get prediction and maximum probabilities for confidence scoring
-        logger.info(f'Getting max {maxprob} probabilities for {lvl}')
-        split_point = aggregated.shape[1] - top_k
-        maxprobs = np.sort(np.partition(aggregated, split_point)[:, split_point:])[::-1]
-        spread = maxprobs[:, 0] - maxprobs[:, 1]
+        logger.info(f'Getting max {top_k} probabilities for {lvl}')
+        maxprobs = torch.topk(aggregated, top_k, dim=1, largest=True, sorted=True).values
 
         # build input matrix for confidence model
         logger.info('Calculating confidence probabilities')
-        conf_input = np.concatenate([lengths[:, np.newaxis], maxprobs, spread[:, np.newaxis]], axis=1, dtype=np.float32)
-        output_data[f'{lvl}_prob'] = conf_model(conf_input)
+        conf_input = torch.column_stack([lengths, maxprobs])
+
+        output_data[f'{lvl}_prob'] = conf_model(conf_input).numpy().squeeze()
 
         # set next left bound for all_levels_aggregated
         s = e
@@ -227,191 +157,7 @@ def run_torchscript_inference(argv=None):
         outf = sys.stdout
     else:
         outf = open(args.output, 'w')
-    output.to_csv(outf, index=False)
+    output.to_csv(outf, index=True)
 
     after = time()
     logger.info(f'Took {after - before:.1f} seconds')
-
-
-
-def run_onnx_inference(argv=None):
-    """
-    Convert a Torch model checkpoint to ONNX format
-    """
-
-    import argparse
-    import json
-    import logging
-    from time import time
-
-    import numpy as np
-    from numpy.lib.stride_tricks import sliding_window_view as swv
-    import pandas as pd
-    import ruamel.yaml as yaml
-    import skbio
-    from skbio.sequence import DNA
-
-
-    from .sequence import FastaReader, FastaSequenceEncoder
-
-    desc = "Run predictions using ONNX"
-    epi = ("")
-
-    parser = argparse.ArgumentParser(description=desc, epilog=epi)
-    parser.add_argument('fastas', nargs='+', type=str, help='the Fasta files to do taxonomic classification on')
-    parser.add_argument('-c', '--n_chunks', type=int, default=10000, help='the number of sequence chunks to process at a time')
-    parser.add_argument('-F', '--fof', action='store_true', default=False, help='a file-of-files was passed in')
-    parser.add_argument('-o', '--output', type=str, default=None, help='the output file to save classifications to')
-    parser.add_argument('-d', '--debug', action='store_true', default=False, help='print specific information about sequences')
-
-    args = parser.parse_args(argv)
-
-    if args.fof:
-        with open(args.fastas[0], 'r') as f:
-            args.fastas = [s.strip() for s in f.readlines()]
-
-    logger = get_logger()
-    if args.debug:
-        logger.setLevel(logging.DEBUG)
-
-
-    model_path, conf_model_path, config_path, tt_path, vocab, elements = _load_deploy_pkg()
-
-    so = ort.SessionOptions()
-    so.intra_op_num_threads = 1
-    so.inter_op_num_threads = 1
-
-    logger.info(f'loading model from {model_path}')
-    nn_sess = ort.InferenceSession(model_path, sess_options=so, providers=['CUDAExecutionProvider'])
-
-    nn_sess.disable_fallback()
-
-    # 'CPUExecutionProvider',
-
-    logger.info(f'loading confidence model from {conf_model_path}')
-    conf_sess = ort.InferenceSession(conf_model_path, sess_options=so, providers=['CUDAExecutionProvider'])
-    n_max_probs = conf_sess.get_inputs()[0].shape[1] - 1
-
-
-    logger.info(f'loading taxonomy table from {tt_path}')
-    tt_df = pd.read_csv(tt_path, index_col=0)
-
-    outcols = ['filename', 'ID'] + tt_df.columns.tolist() + ['score']
-    outcols.remove('taxon_id')
-
-    assert nn_sess.get_outputs()[0].shape[1] == len(tt_df)
-
-    logger.info(f'found {len(tt_df)} taxa')
-
-    window = config['window']
-    step = config['step']
-    k = len(tt_df) - n_max_probs
-
-    all_preds = list()
-    all_maxprobs = list()
-    all_lengths = list()
-    all_seqnames = list()
-    all_filepaths = list()
-    aggregated = list()
-
-    logger.info(f'beginning inference')
-    before = time()
-
-    encoder = FastaSequenceEncoder(window, step, vocab=vocab)
-    reader = FastaReader(encoder)
-
-    n_seqs = 0
-    for file_path, seq_name, seq_len, seq_chunks in reader.read(args.fastas):
-        all_seqnames.append(seq_name)
-        all_lengths.append(seq_len)
-        all_filepaths.append(file_path)
-
-        logger.debug(f'getting outputs for {all_seqnames[-1]}, {len(seq_chunks)} chunks, {all_lengths[-1]} bases')
-        outputs = np.zeros(len(tt_df), dtype=float)   # the output from the network for a single sequence
-        # sum network outputs from all chunks
-        for s in range(0, len(seq_chunks), args.n_chunks):
-            e = s + args.n_chunks
-            outputs += nn_sess.run(None, {'input': seq_chunks[s:e]})[0].sum(axis=0)
-        # divide by the number of seq_chunks we processed to get a mean output
-        outputs /= len(seq_chunks)
-
-        aggregated.append(outputs)
-
-
-    # aggregate everything we just pulled from the fasta file
-    aggregated = np.array(aggregated)
-
-    lvl_prob = list()
-    s = 0
-    for
-
-    # get prediction and maximum probabilities for confidence scoring
-    logger.info('getting max probabilities')
-    preds = np.argmax(aggregated, axis=1)
-    all_maxprobs.append(np.sort(np.partition(aggregated, k)[:, k:])[::-1])
-    all_preds.append(preds)
-
-    # build input matrix for confidence model
-    all_lengths = np.array(all_lengths, dtype=np.float32)
-    all_maxprobs = np.concatenate(all_maxprobs)
-    conf_input = np.concatenate([all_lengths[:, np.newaxis], all_maxprobs], axis=1, dtype=np.float32)
-
-    # get confidence probabilities
-    logger.info('calculating confidence probabilities')
-    conf = conf_sess.run(None, {'float_input': conf_input})[1][:, 1]
-
-    # build the final output data frame
-    logger.info('building final output data frame')
-    all_preds = np.concatenate(all_preds)
-    output = tt_df.iloc[all_preds].copy()
-    output['filename'] = all_filepaths    # ERROR
-    output['ID'] = all_seqnames
-    output['score'] = conf
-    output = output[outcols]
-
-    # write out data
-    if args.output is None:
-        outf = sys.stdout
-    else:
-        outf = open(args.output, 'w')
-    output.to_csv(outf, index=False)
-
-    after = time()
-    logger.info(f'Took {after - before:.1f} seconds')
-
-def main():
-    logging.getLogger().setLevel(logging.INFO)
-    logging.basicConfig(format='%(levelname)s-%(message)s')
-    logging.info('starting')
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--fasta_path', type=str,
-                        default=None, help='sequence path')
-    parser.add_argument('-t', '--txt_file', type=str,
-                        default=None, help='txt file with fasta paths')
-    parser.add_argument('-o', '--output', type=str,
-                        default=None, help='output destination')
-    args = parser.parse_args(argv)
-
-    if args.txt_file:
-        with open(args.txt_file, 'r') as f:
-            fasta_paths = [path.strip() for path in f]
-    else:
-        fasta_paths = [args.fasta_path,]
-
-    for fasta_path in fasta_paths:
-        get_predictions(fasta_path=fasta_path, output_dest=args.output)
-
-    logger.info('finished')
-
-
-def run_test(argv=None):
-    data_path = get_data_path()
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-o', '--output', type=str,
-                        default=None, help='output destination')
-
-    args = parser.parse_args(argv)
-    get_predictions(fasta_path=data_path,
-                    output_dest=args.output)
-
--- gtnet/sequence.py
+++ gtnet/sequence.py
@@ -5,42 +5,7 @@ import numpy as np
 import skbio
 from skbio.sequence import DNA
 
-__all__ = ['_get_DNA_map',
-           'get_sequences',
-           'chunk_seq',
-           'get_rev_seq',
-           'get_bidir_seq',
-           'get_revcomp_map']
-
-
-def get_revcomp_map(vocab):
-    chars = {
-        'A': 'T',
-        'G': 'C',
-        'C': 'G',
-        'T': 'A',
-        'Y': 'R',
-        'R': 'Y',
-        'W': 'W',
-        'S': 'S',
-        'K': 'M',
-        'M': 'K',
-        'D': 'H',
-        'V': 'B',
-        'H': 'D',
-        'B': 'V',
-        'X': 'X',
-        'N': 'N',
-    }
-
-    d = {c: i for i, c in enumerate(vocab)}
-    rcmap = np.zeros(len(vocab), dtype=int)
-    for i, base in enumerate(vocab):
-        rc_base = chars[base]
-        base_i = d[base]
-        rc_base_i = d[rc_base]
-        rcmap[base_i] = rc_base_i
-    return rcmap
+__all__ = ['FastaSequenceEncoder', 'FastaReader']
 
 
 class FastaSequenceEncoder:
@@ -72,8 +37,8 @@ class FastaSequenceEncoder:
             batches[2 * i + 1][:l] = rev[s:e]
         return batches
 
-    @staticmethod
-    def get_dna_map(vocab=None):
+    @classmethod
+    def get_dna_map(cls, vocab=None):
         '''
         Create data structures for mapping DNA sequence to
 
@@ -84,7 +49,7 @@ class FastaSequenceEncoder:
         '''
         if vocab is None:
             vocab = ('ACYWSKDVNTGRMHB')
-        rcmap = get_revcomp_map(vocab)
+        rcmap = cls.get_revcomp_map(vocab)
         basemap = np.zeros(128, dtype=np.uint8)
         # reverse so we store the lowest for self-complementary codes
         for i, c in reversed(list(enumerate(vocab))):
@@ -93,6 +58,35 @@ class FastaSequenceEncoder:
         basemap[ord('x')] = basemap[ord('X')] = basemap[ord('n')]
         return vocab, basemap, rcmap
 
+    @classmethod
+    def get_revcomp_map(cls, vocab):
+        chars = {
+            'A': 'T',
+            'G': 'C',
+            'C': 'G',
+            'T': 'A',
+            'Y': 'R',
+            'R': 'Y',
+            'W': 'W',
+            'S': 'S',
+            'K': 'M',
+            'M': 'K',
+            'D': 'H',
+            'V': 'B',
+            'H': 'D',
+            'B': 'V',
+            'X': 'X',
+            'N': 'N',
+        }
+
+        d = {c: i for i, c in enumerate(vocab)}
+        rcmap = np.zeros(len(vocab), dtype=int)
+        for i, base in enumerate(vocab):
+            rc_base = chars[base]
+            base_i = d[base]
+            rc_base_i = d[rc_base]
+            rcmap[base_i] = rc_base_i
+        return rcmap
 
 
 class FastaReader(mp.Process):
@@ -106,8 +100,7 @@ class FastaReader(mp.Process):
     def _target(self, q, fastas):
         i = 0
         for fa in fastas:
-            print("Reading sequence", i)
-            logging.info(f'loading {fa}')
+            logging.debug(f'loading {fa}')
             for seq in skbio.read(fa, format='fasta', constructor=DNA, validate=False):
                 batches = self.encoder.encode(seq.values)
                 val = (fa, seq.metadata['id'], len(seq), batches)
@@ -116,15 +109,12 @@ class FastaReader(mp.Process):
         q.put(self._done)
 
     def read(self, fastas):
-        print("creating Queue")
         self._q = mp.Queue()
         self._proc = mp.Process(target=self._target, args=(self._q, fastas))
-        print("starting Process")
         self._proc.start()
         return self
 
     def __iter__(self):
-        print("returning self")
         return self
 
     def __next__(self):
@@ -136,96 +126,3 @@ class FastaReader(mp.Process):
                 raise StopIteration
             break
         return seq
-
-
-def _get_DNA_map(chars):
-    '''
-    create a DNA map with some redundancy so that we can
-    do base-complements with +/% operations.
-    Using this scheme, the complement of a base should be:
-    (base_integer + 9) % 18
-    chars[(basemap[ord('N')]+9)%18]
-    For this to work, bases need to be ordered as they are below
-    '''
-    basemap = np.zeros(128, dtype=np.uint8)
-    for i, c in reversed(list(enumerate(chars))):  # reverse so we store the lowest for self-complementary codes
-        basemap[ord(c.upper())] = i
-        basemap[ord(c.lower())] = i
-    basemap[ord('x')] = basemap[ord('X')] = basemap[ord('n')]
-    return basemap
-
-
-class DNAEncoder:
-    '''
-    Creates an Encoder with the provided character sequence
-
-    Converts all (string) characters of DNA to integers
-    based on where the character is in our chars array
-
-    For example, with `chars = "ATCG"`, `A` would map
-    to `0` and `T` would map to `2` and so forth
-
-    Parameters
-    ----------
-    chars : str
-        A string with the DNA characters to encode.
-        The position of the base in the string will
-        be the integer it gets encoded to
-
-    '''
-    def __init__(self, chars):
-        self.chars = chars
-        self.basemap = _get_DNA_map(self.chars)
-
-    def encode(self, seq):
-        '''
-        Encode string characters into associated integers
-
-        Parameters
-        ----------
-        seq : skbio.sequence._dna.DNA
-            A scikit-bio *DNA* object
-
-        Returns
-        -------
-        encoded_sequence : numpy.ndarray
-            Encoded sequence from associated basemap
-
-        '''
-        charar = seq.values.view(np.uint8)
-        encoded_sequence = self.basemap[charar]
-        return encoded_sequence
-
-
-def batch_sequence(sequence, window, padval, step, encoder):
-    '''
-    Takes a DNA sequence of string characters returns an
-    array with the sequence encoded into integers and batched
-    in the same manner as the model was trained on
-
-    Parameters
-    ----------
-    sequence : skbio.sequence._dna.DNA
-        The character DNA sequence, read from file
-    window : int
-        The window size used for batching our sequence
-    padval : int
-        Which value should be used for padding
-    step : int
-        Step size used for batching our sequence
-    encoder : DNAEncoder
-        An instantiation of our DNAEncoder class
-
-    Returns
-    -------
-    batches : numpy.ndarray
-        original sequence broken down into batches
-    '''
-    encoded_seq = encoder.encode(sequence)
-    starts = np.arange(0, encoded_seq.shape[0], step)
-    ends = np.minimum(starts + window, encoded_seq.shape[0])
-    batches = np.ones((len(starts), window), dtype=int) * padval
-    for idx, (start_idx, end_idx) in enumerate(zip(starts, ends)):
-        length = end_idx - start_idx
-        batches[idx][:length] = encoded_seq[start_idx:end_idx]
-    return batches
